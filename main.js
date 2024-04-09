03f2a50afa16dc2d51a8cda07f7c23ccf90c5c8a
PablisMB <pablismb@gmail.com>
2024-04-09 21:44:21 +0200

    better

--- main.js
+++ main.js
@@ -17,9 +17,10 @@ function showSlide(index) {
   if (slides[index] === slides[0]) {
     arrowLeft.style.display = "none";
     arrowRight.style.display = "none";
-  } else {
-    arrowLeft.style.display = "block";
   }
+  // else {
+  //   arrowLeft.style.display = "block";
+  // }
 
   if (slides[index] === slides[slides.length - 1]) {
     arrowRight.style.display = "none";
@@ -52,6 +53,86 @@ function showSlide(index) {
   }
 
   if (slides[index] === slides[4]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[5]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[6]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[7]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[8]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[9]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[10]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[11]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[12]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[13]) {
+    contador.style.display = "block";
+    slides[index].style.height = "90%";
+    slides[index].style.top = "49%";
+    body.style.overflow = "hidden";
+    arrowLeft.style.display = "none";
+  }
+
+  if (slides[index] === slides[14]) {
     contador.style.display = "block";
     slides[index].style.height = "90%";
     slides[index].style.top = "49%";
@@ -235,6 +316,7 @@ function showAnswer10() {
     } else {
       respuesta13.style.backgroundColor = "#91ff02";
     }
+    respuesta.style.color = "#fff";
   });
   arrowRight.style.display = "block";
 }
@@ -280,6 +362,7 @@ function showAnswer20() {
     } else {
       respuesta22.style.backgroundColor = "#91ff02";
     }
+    respuesta.style.color = "#fff";
   });
   arrowRight.style.display = "block";
 }
@@ -325,6 +408,339 @@ function showAnswer30() {
     } else {
       respuesta33.style.backgroundColor = "#91ff02";
     }
+    respuesta.style.color = "#fff";
   });
   arrowRight.style.display = "block";
 }
+
+// Pregunta 4
+const respuesta41 = document.getElementById("41");
+const respuesta42 = document.getElementById("42");
+
+// Correcta 41
+respuesta41.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer40();
+});
+
+function showAnswer40() {
+  const respuestas40 = document.querySelectorAll(".pregunta-button40");
+  respuestas40.forEach((respuesta) => {
+    if (respuesta === respuesta41) {
+      respuesta.style.pointerEvents = "none";
+      respuesta41.style.pointerEvents = "none";
+      respuesta42.style.pointerEvents = "none";
+      respuesta41.style.backgroundColor = "#91ff02";
+      respuesta42.style.backgroundColor = "red";
+      arrowRight.style.display = "block";
+    }
+    respuesta.style.color = "#fff";
+  });
+}
+
+// Pregunta 5
+const respuesta51 = document.getElementById("51");
+const respuesta52 = document.getElementById("52");
+const respuesta53 = document.getElementById("53");
+const respuesta54 = document.getElementById("54");
+
+// Correcta all
+
+respuesta51.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer50();
+});
+
+respuesta52.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer50();
+});
+
+respuesta53.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer50();
+});
+
+respuesta54.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer50();
+});
+
+function showAnswer50() {
+  const respuestas50 = document.querySelectorAll(".pregunta-button50");
+  respuestas50.forEach((respuesta) => {
+    respuesta.style.color = "#fff";
+    respuesta.style.pointerEvents = "none";
+    respuesta.style.backgroundColor = "#91ff02";
+    arrowRight.style.display = "block";
+  });
+}
+
+// Pregunta 6
+const respuesta61 = document.getElementById("61");
+const respuesta62 = document.getElementById("62");
+const respuesta63 = document.getElementById("63");
+const respuesta64 = document.getElementById("64");
+
+// Correcta 63
+respuesta61.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer60();
+});
+
+respuesta62.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer60();
+});
+
+respuesta63.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer60();
+});
+
+respuesta64.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer60();
+});
+
+function showAnswer60() {
+  const respuestas60 = document.querySelectorAll(".pregunta-button60");
+  respuestas60.forEach((respuesta) => {
+    if (respuesta !== respuesta63) {
+      respuesta.style.pointerEvents = "none";
+      respuesta63.style.pointerEvents = "none";
+      respuesta.style.backgroundColor = "red";
+    } else {
+      respuesta63.style.backgroundColor = "#91ff02";
+    }
+    respuesta.style.color = "#fff";
+  });
+  arrowRight.style.display = "block";
+}
+
+// Pregunta 7
+const respuesta71 = document.getElementById("71");
+const respuesta72 = document.getElementById("72");
+const respuesta73 = document.getElementById("73");
+const respuesta74 = document.getElementById("74");
+
+// Correcta 71 y 74
+respuesta71.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer70();
+});
+
+respuesta72.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer70();
+});
+
+respuesta73.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer70();
+});
+
+respuesta74.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer70();
+});
+
+function showAnswer70() {
+  const respuestas70 = document.querySelectorAll(".pregunta-button70");
+  respuestas70.forEach((respuesta) => {
+    if (respuesta === respuesta71 || respuesta === respuesta74) {
+      respuesta.style.pointerEvents = "none";
+      respuesta71.style.pointerEvents = "none";
+      respuesta74.style.pointerEvents = "none";
+      respuesta71.style.backgroundColor = "#91ff02";
+      respuesta74.style.backgroundColor = "#91ff02";
+    } else {
+      respuesta.style.backgroundColor = "red";
+    }
+    respuesta.style.color = "#fff";
+  });
+  arrowRight.style.display = "block";
+}
+
+// Pregunta 8
+const respuesta81 = document.getElementById("81");
+const respuesta82 = document.getElementById("82");
+const respuesta83 = document.getElementById("83");
+const respuesta84 = document.getElementById("84");
+
+// Correcta 81
+respuesta81.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer80();
+});
+
+respuesta82.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer80();
+});
+
+respuesta83.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer80();
+});
+
+respuesta84.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer80();
+});
+
+function showAnswer80() {
+  const respuestas80 = document.querySelectorAll(".pregunta-button80");
+  respuestas80.forEach((respuesta) => {
+    if (respuesta === respuesta81) {
+      respuesta.style.pointerEvents = "none";
+      respuesta81.style.pointerEvents = "none";
+      respuesta82.style.pointerEvents = "none";
+      respuesta83.style.pointerEvents = "none";
+      respuesta84.style.pointerEvents = "none";
+      respuesta81.style.backgroundColor = "#91ff02";
+      respuesta82.style.backgroundColor = "red";
+      respuesta83.style.backgroundColor = "red";
+      respuesta84.style.backgroundColor = "red";
+      arrowRight.style.display = "block";
+    }
+    respuesta.style.color = "#fff";
+  });
+}
+
+// Pregunta 9
+const respuesta91 = document.getElementById("91");
+const respuesta92 = document.getElementById("92");
+const respuesta93 = document.getElementById("93");
+const respuesta94 = document.getElementById("94");
+
+// Correcta all
+respuesta91.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer90();
+});
+
+respuesta92.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer90();
+});
+
+respuesta93.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer90();
+});
+
+respuesta94.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer90();
+});
+
+function showAnswer90() {
+  const respuestas90 = document.querySelectorAll(".pregunta-button90");
+  respuestas90.forEach((respuesta) => {
+    respuesta.style.color = "#fff";
+    respuesta.style.pointerEvents = "none";
+    respuesta.style.backgroundColor = "#91ff02";
+    arrowRight.style.display = "block";
+  });
+}
+
+// Pregunta 10
+const respuesta101 = document.getElementById("101");
+const respuesta102 = document.getElementById("102");
+const respuesta103 = document.getElementById("103");
+const respuesta104 = document.getElementById("104");
+
+// Correcta 104
+respuesta101.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer100();
+});
+
+respuesta102.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer100();
+});
+
+respuesta103.addEventListener("click", () => {
+  contadorValue--;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer100();
+});
+
+respuesta104.addEventListener("click", () => {
+  contadorValue++;
+  contadorValueCont.textContent = contadorValue + " / 8";
+  showAnswer100();
+});
+
+function showAnswer100() {
+  const respuestas100 = document.querySelectorAll(".pregunta-button100");
+  respuestas100.forEach((respuesta) => {
+    if (respuesta !== respuesta104) {
+      respuesta.style.pointerEvents = "none";
+      respuesta104.style.pointerEvents = "none";
+      respuesta.style.backgroundColor = "red";
+    } else {
+      respuesta104.style.backgroundColor = "#91ff02";
+    }
+    respuesta.style.color = "#fff";
+  });
+  arrowRight.style.display = "block";
+}
+
+const numericInput = document.getElementById("numericInput");
+const sendButton = document.getElementById("sendButton");
+const intentosValue = document.querySelector(".intentos-value");
+let intentos = 3;
+intentosValue.textContent = intentos;
+
+sendButton.addEventListener("click", () => {
+  const numericValue = numericInput.value;
+  if (numericValue === "2224") {
+    contadorValue++;
+    contadorValue++;
+    contadorValue++;
+    contadorValueCont.textContent = "120923" + " / 8";
+    numericInput.style.pointerEvents = "none";
+    numericInput.style.backgroundColor = "#91ff02";
+    sendButton.style.pointerEvents = "none";
+    sendButton.style.backgroundColor = "#91ff02";
+    arrowRight.style.display = "block";
+  } else {
+    intentos--;
+    numericInput.value = "";
+    intentosValue.textContent = intentos;
+    if (intentos === 0) {
+      numericInput.style.pointerEvents = "none";
+      numericInput.style.backgroundColor = "red";
+      sendButton.style.pointerEvents = "none";
+      sendButton.style.backgroundColor = "red";
+      contadorValueCont.textContent = "120923" + " / 8";
+      arrowRight.style.display = "block";
+    }
+  }
+});
